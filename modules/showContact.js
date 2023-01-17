98d5b9e349b6caabcdc2eccf00cf6bf67c08cc40
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-17 16:21:13 +0100

    Re-create your Awesome books app with ES6

---
+++ modules/showContact.js
@@ -0,0 +1,17 @@
+export default function showContact() {
+    const list = document.querySelector('.list');
+    const addNew = document.querySelector('.add_new');
+    const contact = document.querySelector('.contact');
+    const listBtn = document.querySelector('.listBtn');
+    const addNewBtn = document.querySelector('.add_newBtn');
+    const contactBtn = document.querySelector('.contactBtn');
+
+    contactBtn.addEventListener('click', () => {
+        list.classList.add('hidden');
+        addNew.classList.add('hidden');
+        contact.classList.remove('hidden');
+        listBtn.style.color = 'black';
+        addNewBtn.style.color = 'black';
+        contactBtn.style.color = 'rgb(55, 135, 172)';
+    });
+}
---
+++ modules/showList.js
@@ -0,0 +1,17 @@
+export default function showList() {
+    const list = document.querySelector('.list');
+    const addNew = document.querySelector('.add_new');
+    const contact = document.querySelector('.contact');
+    const listBtn = document.querySelector('.listBtn');
+    const addNewBtn = document.querySelector('.add_newBtn');
+    const contactBtn = document.querySelector('.contactBtn');
+
+    listBtn.addEventListener('click', () => {
+        list.classList.remove('hidden');
+        addNew.classList.add('hidden');
+        contact.classList.add('hidden');
+        listBtn.style.color = 'rgb(55, 135, 172)';
+        addNewBtn.style.color = 'black';
+        contactBtn.style.color = 'black';
+    });
+}
