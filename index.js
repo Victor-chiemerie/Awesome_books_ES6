163a0744b57065d2141f2b58a2b3140fc5424bd8
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-17 16:45:26 +0100

    fix linters error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Library from "./modules/book.js"
-import showList from "./modules/showList.js";
-import showAddNew from "./modules/showAddNew.js";
-import showContact from "./modules/showContact.js";
+import Library from './modules/book.js';
+import showList from './modules/showList.js';
+import showAddNew from './modules/showAddNew.js';
+import showContact from './modules/showContact.js';
 import { DateTime } from './modules/luxon.js';
 
 // The funtions below display the header
@@ -11,10 +11,9 @@ showContact();
 
 // Display the time
 setInterval(() => {
-    const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-    document.getElementById('date').innerHTML = currentDate;
-  }, 1000);
-
+  const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+  document.getElementById('date').innerHTML = currentDate;
+}, 1000);
 
 const booklist = document.querySelector('#booklist');
 const inputTitle = document.getElementById('title');
@@ -24,36 +23,36 @@ const buttonAdd = document.getElementById('add_button');
 const obj = new Library();
 
 function printBooks() {
-    const shelf = JSON.parse(localStorage.getItem('shelf')) || [];
-    let innerhtml = '';
+  const shelf = JSON.parse(localStorage.getItem('shelf')) || [];
+  let innerhtml = '';
 
-    shelf.forEach((book, index) => {
-        innerhtml += `
+  shelf.forEach((book, index) => {
+    innerhtml += `
     <div>
         <p>"${book.title}" by ${book.author} </p>
         <button id="remove-btn${index}">Remove</button>
             </div>
         `;
-    });
+  });
 
-    booklist.innerHTML = innerhtml;
+  booklist.innerHTML = innerhtml;
 
-    shelf.forEach((book, index) => {
-        const removeBtn = document.getElementById(`remove-btn${index}`);
-        removeBtn.addEventListener('click', () => {
-            obj.removeBook(index);
-            printBooks();
-        });
+  shelf.forEach((book, index) => {
+    const removeBtn = document.getElementById(`remove-btn${index}`);
+    removeBtn.addEventListener('click', () => {
+      obj.removeBook(index);
+      printBooks();
     });
+  });
 }
 
 printBooks();
 
 buttonAdd.addEventListener('click', (event) => {
-    event.preventDefault();
-    obj.addBook(inputTitle.value, inputAuthor.value);
-    printBooks();
+  event.preventDefault();
+  obj.addBook(inputTitle.value, inputAuthor.value);
+  printBooks();
 
-    inputTitle.value = '';
-    inputAuthor.value = '';
+  inputTitle.value = '';
+  inputAuthor.value = '';
 });
--- modules/showAddNew.js
+++ modules/showAddNew.js
@@ -1,17 +1,17 @@
 export default function showAddNew() {
-    const list = document.querySelector('.list');
-    const addNew = document.querySelector('.add_new');
-    const contact = document.querySelector('.contact');
-    const listBtn = document.querySelector('.listBtn');
-    const addNewBtn = document.querySelector('.add_newBtn');
-    const contactBtn = document.querySelector('.contactBtn');
+  const list = document.querySelector('.list');
+  const addNew = document.querySelector('.add_new');
+  const contact = document.querySelector('.contact');
+  const listBtn = document.querySelector('.listBtn');
+  const addNewBtn = document.querySelector('.add_newBtn');
+  const contactBtn = document.querySelector('.contactBtn');
 
-    addNewBtn.addEventListener('click', () => {
-        list.classList.add('hidden');
-        addNew.classList.remove('hidden');
-        contact.classList.add('hidden');
-        listBtn.style.color = 'black';
-        addNewBtn.style.color = 'rgb(55, 135, 172)';
-        contactBtn.style.color = 'black';
-    });
+  addNewBtn.addEventListener('click', () => {
+    list.classList.add('hidden');
+    addNew.classList.remove('hidden');
+    contact.classList.add('hidden');
+    listBtn.style.color = 'black';
+    addNewBtn.style.color = 'rgb(55, 135, 172)';
+    contactBtn.style.color = 'black';
+  });
 }
